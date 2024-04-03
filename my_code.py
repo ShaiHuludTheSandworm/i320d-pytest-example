1e2b21fa2ec59caaa486af13c54d3fd5b1db42be
ShaiHuludTheSandworm <145036835+ShaiHuludTheSandworm@users.noreply.github.com>
2024-04-03 16:48:12 -0500

    Update my_code.py

--- my_code.py
+++ my_code.py
@@ -21,6 +21,7 @@ def test_fix_phone_num():
 def test_dash_fix_phone_num():
   assert fix_phone_num("555-442-98761") == '(555) 442 98761'
   assert fix_phone_num("(321) 654 3333") == '(321) 654 3333'
+  
 def test_error_fix_phone_num():
-  assert fix_phone_num("32165433333") == '(321) 654 3333'
   with pytest.raises(ValueError):
+    fix_phone_num("32165433333")
