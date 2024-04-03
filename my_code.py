d2dc98f94c368b7a757d757788cf7f8d55a97e55
ShaiHuludTheSandworm <145036835+ShaiHuludTheSandworm@users.noreply.github.com>
2024-04-03 16:35:47 -0500

    Update my_code.py

--- my_code.py
+++ my_code.py
@@ -9,14 +9,15 @@ def fix_phone_num(phone_num_to_fix):
   fixed_num = "(" + area_code + ")" + " " + three_part + " " + four_part 
   if (len(phone_num_to_fix) != 10):
     raise ValueError("Can only format numbers that are exactly 10 digits long")
-  if not phone_num_to_fix.isdigit():
-    raise ValueError("Can only format numbers that are numerical")
   return fixed_num
 
 def test_fix_phone_num():
   assert fix_phone_num("5125558823") == '(512) 555 8823'
   assert fix_phone_num("5554429876") == '(555) 442 9876'
-  assert fix_phone_num("32165433a") == '(321) 654 3333'
+  assert fix_phone_num("3216543333") == '(321) 654 3333'
   # Now check that a too short string gives a ValueError
   with pytest.raises(ValueError):
     fix_phone_num("51")
+def test_dash_fix_phone_num():
+  assert fix_phone_num("555-442-98761") == '(555) 442 98761'
+  assert fix_phone_num("(321) 654 3333") == '(321) 654 3333'
