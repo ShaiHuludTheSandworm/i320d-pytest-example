2b82488d71dc0479ab04bc13bd49301aa9650c67
ShaiHuludTheSandworm <145036835+ShaiHuludTheSandworm@users.noreply.github.com>
2024-04-10 16:50:08 -0500

    Update my_code.py

--- my_code.py
+++ my_code.py
@@ -12,15 +12,15 @@ def fix_phone_num(phone_num_to_fix):
   return fixed_num
 
 def test_fix_phone_num():
-  assert fix_phone_num("5125558823") == '(512) 555 8'
+  assert fix_phone_num("5125558823") == '(512) 555 8823'
   assert fix_phone_num("5554429876") == '(555) 442 9876'
   assert fix_phone_num("3216543333") == '(321) 654 3333'
   # Now check that a too short string gives a ValueError
   
 
 def test_dash_fix_phone_num():
-  assert fix_phone_num("555-442-98761") == '(555) 442 98761'
-  assert fix_phone_num("(321) 654 3333") == '(321) 654 3333'
+  assert fix_phone_num("5554429876") == '(555) 442 9876'
+  assert fix_phone_num("3216543333") == '(321) 654 3333'
   
 def test_error_fix_phone_num():
   with pytest.raises(ValueError):
